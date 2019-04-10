7c870b86b26bb7da62cd85c39aa1790027560606
Ivana <dzunja91@gmail.com>
2019-04-10 21:07:49 +0200

    css fix

--- js/slider.js
+++ js/slider.js
@@ -13,7 +13,7 @@
     speed: 600,
     fade: true,
     cssEase: 'linear',
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000
   });
 
@@ -52,32 +52,35 @@
 
 
   $(document).ready(function() {
-	
-    // INITIATE THE FOOTER
-    siteFooter();
-    // COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
-    $(window).on('load',function() {
-      siteFooter();
-    });
     
-    function siteFooter() {
-      var siteContent = $('#site-content');
-      var siteContentHeight = siteContent.height();
-      var siteContentWidth = siteContent.width();
-  
-      var siteFooter = $('#site-footer');
-      var siteFooterHeight = siteFooter.height();
-      var siteFooterWidth = siteFooter.width();
-  
-      console.log('Content Height = ' + siteContentHeight + 'px');
-      console.log('Content Width = ' + siteContentWidth + 'px');
-      console.log('Footer Height = ' + siteFooterHeight + 'px');
-      console.log('Footer Width = ' + siteFooterWidth + 'px');
-  
-      siteContent.css({
-        "margin-bottom" : siteFooterHeight + 150
+    if($(window).width() >= 768) {      
+      // INITIATE THE FOOTER
+      siteFooter();
+      // COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+      $(window).on('load',function() {
+        siteFooter();
       });
-    };
+      
+      function siteFooter() {
+        var siteContent = $('#site-content');
+        var siteContentHeight = siteContent.height();
+        var siteContentWidth = siteContent.width();
+    
+        var siteFooter = $('#site-footer');
+        var siteFooterHeight = siteFooter.height();
+        var siteFooterWidth = siteFooter.width();
+    
+        // console.log('Content Height = ' + siteContentHeight + 'px');
+        // console.log('Content Width = ' + siteContentWidth + 'px');
+        // console.log('Footer Height = ' + siteFooterHeight + 'px');
+        // console.log('Footer Width = ' + siteFooterWidth + 'px');
+    
+        siteContent.css({
+          "margin-bottom" : siteFooterHeight + 150
+        });
+      };
+    }
+   
   });
 
 
